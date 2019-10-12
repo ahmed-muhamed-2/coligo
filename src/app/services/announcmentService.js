c37edf37c292219e802e3caf473c8794ff2aaa21
ahmed-muhamed-2 <ahmed.muhmd.93+github@gmail.com>
2019-10-12 21:52:09 +0200

    Add real service to (announcment & quizes)

---
+++ src/app/services/announcmentService.js
@@ -0,0 +1,10 @@
+import { http } from './httpServices';
+
+const ANNOUNCMENT_SERVICE = 'http://test.coligolms.com/api/announcement';
+// const ANNOUNCMENT_SERVICE = 'https://jsonplaceholder.typicode.com/todos';
+const LISTANNOUNCMENT = http.get(ANNOUNCMENT_SERVICE);
+
+export default LISTANNOUNCMENT;
+
+
+
